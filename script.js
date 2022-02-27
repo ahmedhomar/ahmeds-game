c5081607ba9e9365bc3c0bcec5bee3fe6b693015
Ahmed Omar <ahmedomar@hey.com>
2022-02-27 19:49:04 +0000

    changes to event handlers and svg images

--- script.js
+++ script.js
@@ -1,69 +1,62 @@
 // Variables
 
-const farmerNorth = document.querySelector("#farmer-north");
 const wolfNorth = document.querySelector("#wolf-north");
 const goatNorth = document.querySelector("#goat-north");
-const boatNorth = document.querySelector("#boat-north");
+// const boatNorth = document.querySelector("#boat-north");
 const pumpkinNorth = document.querySelector("#pumpkin-north");
-
-const farmerSouth = document.querySelector("#farmer-south");
 const wolfSouth = document.querySelector("#wolf-south");
 const goatSouth = document.querySelector("#goat-south");
-const boatSouth = document.querySelector("#boat-south");
+// const boatSouth = document.querySelector("#boat-south");
 const pumpkinSouth = document.querySelector("#pumpkin-south");
-
-const northBankArray = [];
-const SouthBankArray = [];
+let tripsTaken = document.querySelector("#trips");
+// const northBankArray = [];
+// const SouthBankArray = [];
+let tripCounter = 0;
 
 // Functions
 
-farmerNorth.addEventListener("click", (e) => {
-  console.log("Something is coming");
-  northBankArray.push("farmer");
-  return (farmerNorth.innerHTML = "");
-});
-
-wolfNorth.addEventListener("click", (e) => {
+wolfNorth.addEventListener("click", () => {
   console.log("No wolf!");
-  northBankArray.push("wolf");
-
-  return (wolfNorth.innerHTML = "");
+  tripsTaken.innerHTML += "* ";
+  wolfNorth.innerHTML = "";
+  wolfSouth.innerHTML = `<img src="./images/wolf-1.svg" />`;
+  if (goatNorth.innerHTML && pumpkinNorth.innerHTML != "") {
+    return alert(`GAME OVER:Pumpkin eaten by goat!`);
+  } else {
+  }
 });
 
-goatNorth.addEventListener("click", (e) => {
+goatNorth.addEventListener("click", () => {
   console.log("Goat has gone!");
-  northBankArray.push("goat");
-  return (goatNorth.innerHTML = "");
+  tripsTaken.innerHTML += "* ";
+  goatNorth.innerHTML = "";
+  return (goatSouth.innerHTML = `<img src="./images/sheep.svg" />`);
 });
 
 pumpkinNorth.addEventListener("click", () => {
   console.log("pumpkin has gone!");
-  northBankArray.push("pumpkin");
+  tripsTaken.innerHTML += "* ";
   pumpkinNorth.innerHTML = "";
-  return (pumpkinSouth.innerHTML = `
-  <figure id="pumpkin-north">
-      <img src="./images/pumpkin-1.svg"/>
-      <figcaption>Pumpkin</figcaption>
-  </figure>`);
+  return (pumpkinSouth.innerHTML = `<img src="./images/pumpkin-1.svg"/>`);
 });
 
-boatNorth.addEventListener("click", () => {
-  if (northBankArray.length <= 2) {
-    console.log("This ship has sailed!");
-    return (boatNorth.innerHTML = "");
-  } else if (northBankArray.length >= 3) {
-    alert("Not enough room in the boat!");
-  } else {
-    console.log("nothing is happening");
-  }
-  anime({
-    targets: boatNorth,
-    translateX: 1500,
-    direction: "alternate",
-    loop: false,
-    easing: "steps(20)",
-  });
-});
+// boatNorth.addEventListener("click", () => {
+//   anime({
+//     targets: boatNorth,
+//     translateX: 1500,
+//     direction: "alternate",
+//     loop: false,
+//     easing: "steps(20)",
+//   });
+//   // if (northBankArray.length <= 2) {
+//   //   console.log("This ship has sailed!");
+//   //   return (boatNorth.innerHTML = "");
+//   // } else if (northBankArray.length >= 3) {
+//   //   alert("Not enough room in the boat!");
+//   // } else {
+//   //   console.log("nothing is happening");
+//   // }
+// });
 
 // let currentPlayer = 1;
 
@@ -89,7 +82,31 @@ boatNorth.addEventListener("click", () => {
 // There is only one boat available which can support the farmer and either of the goat, wolf or the cabbage. So at a time, the boat can have only two objects (farmer and one other).
 
 // If the goat and wolf are left alone (either in the boat or onshore), the wolf will eat the goat.
+
+// if (goatNorth.innerHTML && wolfNorth.innerHTML === "" || boatNorth.innerHTML && pumpkinNorth.innerHTML === "") {
+// goatNorth.innerHTML = "`<img src="./images/blood.svg">`";
+// alert("`<img src="./images/game-over.svg"> Goat eaten by wolf!`")
+
+// }
 // Similarly, if the Goat and cabbage are left alone, then goat will eat the cabbage.
+// if (goatNorth.innerHTML && pumpkinNorth.innerHTML === "" || boatNorth.innerHTML && wolfNorth.innerHTML === "") {
+// goatNorth.innerHTML = "`<img src="./images/apple.svg">`";
+// alert("`<img src="./images/game-over.svg"> Pumpkin eaten by goat!`")
+
+// }
+
+// if (goatSouth.innerHTML && wolfSouth.innerHTML === "" || boatSouth.innerHTML && pumpkinNorth.innerHTML === "") {
+// goatNorth.innerHTML = "`<img src="./images/blood.svg">`";
+// alert("`<img src="./images/game-over.svg"> Goat eaten by wolf!`")
+
+// }
+// Similarly, if the Goat and cabbage are left alone, then goat will eat the cabbage.
+// if (goatNorth.innerHTML && pumpkinNorth.innerHTML === "" || boatNorth.innerHTML && wolfNorth.innerHTML === "") {
+// goatNorth.innerHTML = "`<img src="./images/apple.svg">`";
+// alert("`<img src="./images/game-over.svg"> Pumpkin eaten by goat!`")
+
+// }
+
 // The farmer wants to cross the river with all three of his belongings: goat, wolf, and cabbage.
 // What strategy should he use to do so?
 
